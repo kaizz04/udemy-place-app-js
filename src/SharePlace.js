c3c0b4a6ea22cd4f505dc5206e340f777f3f87cc
kaizz04 <kaveesh@marketpushapps.com>
2024-09-17 15:36:07 +0530

    feature - Convert user address to location

--- src/SharePlace.js
+++ src/SharePlace.js
@@ -1,5 +1,6 @@
 import { Modal } from "./UI/Modal";
 import { Map } from "./UI/Map";
+import { getCoordsFromAddress } from "./Utility/Location";
 
 class PlaceFinder {
   constructor() {
@@ -12,8 +13,7 @@ class PlaceFinder {
 
   selectPlace(coordinates) {
     if (this.map) {
-
-      this.map.render(coordinates); 
+      this.map.render(coordinates);
     } else {
       this.map = new Map(coordinates);
     }
@@ -53,8 +53,26 @@ class PlaceFinder {
     );
   }
 
-  findAddressHandler() {
+  async findAddressHandler(event) {
+    event.preventDefault();
+    const address = event.target.querySelector("input").value;
+    if (!address || address.trim().length === 0) {
+      alert("Invalid address entered - please try again");
+      return;
+    }
 
+    const modal = new Modal(
+      "loading-modal-content",
+      "Loading location - please wait"
+    );
+    modal.show();
+    try {
+      const coordinates = await getCoordsFromAddress(address).then();
+      this.selectPlace(coordinates);
+    } catch (error) {
+      alert(error.message);
+    }
+    modal.hide();
   }
 }
 
---
+++ src/Utility/Location.js
@@ -0,0 +1,18 @@
+const Google_API_Key = "AIzaSyC9vMpO_isbkbxXzlaGDnE_IhbGcbaB67k";
+export async function getCoordsFromAddress(address) {
+  const urlAddress = encodeURI(address);
+  const response = await fetch(
+    `https://maps.googleapis.com/maps/api/geocode/json?address=${urlAddress},+CA&key=${Google_API_Key}`
+  );
+  if (!response.ok) {
+    throw new Error("Failed to fetch coordinates. Please try again!");
+  }
+  const data = await response.json();
+
+  if(data.error_message){
+    throw new Error(data.error_message);
+
+  }
+ const coordinates = data.results[0].geometry.location;
+ return coordinates;
+}
